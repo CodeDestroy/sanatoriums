b82fd1ecd64cdc3d3f1e8697e5c6347d491f81e9
Andrey <andrei_novichihin@mail.ru>
2025-05-20 18:19:03 +0300

    login modal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -164,19 +164,95 @@
                                 <svg class="fill-red-700 hover:fill-red-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                                     <path d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z"></path>
                                 </svg>
-                                </span>
-                            </a>
-                        <button class="focus:outline-none text-white font-somebold  bg-red-700 hover:bg-red-700 focus:ring-4 focus:ring-red-300 rounded-lg
-                         text-sm px-4 py-2   dark:bg-red-700 dark:hover:bg-red-700 dark:focus:ring-red-700">
+                            </span>
+                        </a>
+                        {{-- <button class="focus:outline-none text-white font-somebold  bg-red-700 hover:bg-red-700 focus:ring-4 focus:ring-red-300 rounded-lg
+                            text-sm px-4 py-2   dark:bg-red-700 dark:hover:bg-red-700 dark:focus:ring-red-700" onClick='showLoginModal()'>
                            Войти
                         </button>
+                        
                         <button class="focus:outline-none text-white bg-red-700 hover:bg-red-70 font-somebold0 focus:ring-4 focus:ring-red-300  rounded-lg
-                         text-sm px-4 py-2  dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-700">
+                            text-sm px-4 py-2  dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-700">
                             Регистрация
-                        </button>
+                        </button> --}}
+                        <!-- Обертка с Alpine.js -->
+                        <div x-data="{ openLoginModal: false }">
+                        <!-- Кнопка "Войти" -->
+                            <button @click="openLoginModal = true"
+                                class="focus:outline-none text-white font-somebold bg-red-700 hover:bg-red-700 focus:ring-4 focus:ring-red-300 rounded-lg
+                                    text-sm px-4 py-2 dark:bg-red-700 dark:hover:bg-red-700 dark:focus:ring-red-700">
+                                Войти
+                            </button>
+
+                            <!-- Кнопка "Регистрация" -->
+                            <button class="focus:outline-none text-white bg-red-700 hover:bg-red-70 font-somebold0 focus:ring-4 focus:ring-red-300 rounded-lg
+                                    text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-700">
+                                Регистрация
+                            </button>
+
+                            <!-- Контейнер, для примера -->
+                            <div class="bg-gray-300" ></div>
+
+                            <!-- Модальное окно -->
+                            <div x-show="openLoginModal" x-transition x-cloak class="fixed inset-0 z-20" aria-labelledby="modal-title" aria-modal="true">
+                                <!-- Затемнение фона -->
+                                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" @click="openLoginModal = false"></div>
+
+                                <!-- Контейнер модалки -->
+                                <div class="fixed inset-0 z-30 flex items-center justify-center p-4">
+                                    <div @click.away="openLoginModal = false"
+                                        x-show="openLoginModal"
+                                        x-transition:enter="ease-out duration-300"
+                                        x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                        x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                                        x-transition:leave="ease-in duration-200"
+                                        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+                                        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                        class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                                        
+                                        <!-- Содержимое -->
+                                        <div class="sm:flex sm:items-start">
+                                            <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                                </svg>
+                                            </div>
+                                            <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                                <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Вход в аккаунт</h3>
+                                                <div class="mt-2">
+                                                    <p class="text-sm text-gray-500">Введите свои учетные данные для входа.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Кнопки -->
+                                        <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                                            <button type="button"
+                                                    class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                                    @click="openLoginModal = false">
+                                                Войти
+                                            </button>
+                                            <button type="button"
+                                                    class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                                    @click="openLoginModal = false">
+                                                Отмена
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        
                     </div>
                 </div>
-            
+                
+                <script>
+                    function showLoginModal() {
+                        console.log('show modal')
+                    }    
+                </script>
                 <div class="hidden lg:flex lg:flex-1 lg:justify-end">
                     {{-- @guest
                     <div class="relative" x-data="{ expanded: false }" >
@@ -479,4 +555,5 @@
         </footer> 
     </div>
 </body>
+
 </html>
