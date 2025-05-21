c8b235c8b02bb42a6dac3b0dc76d5554ee252bb1
Andrey <andrei_novichihin@mail.ru>
2025-05-21 13:11:50 +0300

    build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -633,8 +633,8 @@
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                         </div>
-                        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-800  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:border-red-800  focus:border-red-800 " placeholder="Санаторий им.Горького" required />
-                        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-800 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 hover:border-red-800  focus:border-red-800">Поиск</button>
+                        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-800  hover:border-red-800  focus:border-red-800 " placeholder="Санаторий им.Горького" required />
+                        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-800 font-medium rounded-lg text-sm px-4 py-2  hover:border-red-800  focus:border-red-800">Поиск</button>
                     </div>
                 </form>
             </div>
